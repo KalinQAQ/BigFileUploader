7b87a2fa1789a3c5eb33ed5444c913b054e1144d
Kalin <1026486353@qq.com>
2023-12-10 15:30:57 +0800

    🎉 init(FileUploader): 前端项目初始化

---
+++ src/FileUploader.js
@@ -0,0 +1,10 @@
+import { InboxOutlined } from "@ant-design/icons";
+
+function FileUploader() {
+  return (
+    <div className="upload-container">
+      <InboxOutlined />
+    </div>
+  );
+}
+export default FileUploader;
