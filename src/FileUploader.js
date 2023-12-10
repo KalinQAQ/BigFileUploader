86c8bbcf61bedb635edc89c7213e4c46ec21cefc
Kalin <1026486353@qq.com>
2023-12-10 22:52:22 +0800

    ✨ feat(FileUploader): 实现文件的拖拽预览

--- src/FileUploader.js
+++ src/FileUploader.js
@@ -1,10 +1,35 @@
 import { InboxOutlined } from "@ant-design/icons";
+import "./FileUploader.css";
+import { useRef } from "react";
+import useDrag from "./useDrag";
 
 function FileUploader() {
+  const uploadContainerRef = useRef(null);
+  const { selectedFile, filePreview } = useDrag(uploadContainerRef);
   return (
-    <div className="upload-container">
-      <InboxOutlined />
-    </div>
+    <>
+      <div className="upload-container" ref={uploadContainerRef}>
+        {renderFilePrevire(filePreview)}
+      </div>
+    </>
   );
+}
+/**
+ * 显示文件的预览信息
+ * @param {*} filePreview
+ */
+function renderFilePrevire(filePreview) {
+  const { url, type } = filePreview;
+  if (url) {
+    if (type.startsWith("video/")) {
+      return <video src={url} controls />;
+    } else if (type.startsWith("image/")) {
+      return <img src={url} alt="preview" />;
+    } else {
+      return url;
+    }
+  } else {
+    return <InboxOutlined />;
+  }
 }
 export default FileUploader;
