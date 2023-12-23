b1df5a2ff778d8c36ffbc2a423f50fd53224dd47
Kalin <1026486353@qq.com>
2023-12-23 23:31:54 +0800

    服务器基本搭建

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,22 +1,20 @@
 import axios from "axios";
-
 const axiosInstance = axios.create({
-  baseURL: "http://loaclhost:8080",
+  baseURL: "http://localhost:8080",
 });
-
 axiosInstance.interceptors.response.use(
-  // response响应对象,data,headers
-  // response.data.success为true表示成功,为false表示失败了
   (response) => {
+    //response响应对象 data,headers
+    //response.data.success为true表示成功，为false表示失败了
     if (response.data && response.data.success) {
-      return response.data; // 返回响应体，这也的话可以再代码直接获取响应体
+      return response.data; //返回响应体，这样的话可以在代码直接获取响应体
     } else {
       throw new Error(response.data.message || "服务器端错误");
     }
   },
   (error) => {
-    console.log("请求错误", error);
+    console.error("错误", error);
+    throw error;
   }
 );
-
 export default axiosInstance;
